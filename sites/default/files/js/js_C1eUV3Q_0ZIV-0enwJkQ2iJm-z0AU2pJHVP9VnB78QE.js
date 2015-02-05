3cc57594bf47dec62dcfe3d7794e78416e6e5afe
Daniel Deitschel <daniel@gocosmonaut.com>
2015-02-05 15:33:12 -0800

    IE8 style sheet
    431

---
+++ sites/default/files/js/js_C1eUV3Q_0ZIV-0enwJkQ2iJm-z0AU2pJHVP9VnB78QE.js
@@ -0,0 +1,277 @@
+/**
+ * @file
+ * Views Slideshow Xtra Javascript.
+ */
+(function ($) {
+  Drupal.viewsSlideshowXtra = Drupal.viewsSlideshowXtra || {};
+  var pageX = 0, pageY = 0, timeout;
+  Drupal.viewsSlideshowXtra.transitionBegin = function (options) {
+
+    // Find our views slideshow xtra elements
+    $('[id^="views-slideshow-xtra-"]:not(.views-slideshow-xtra-processed)').addClass('views-slideshow-xtra-processed').each(function() {
+
+      // Get the slide box.
+      var slideArea = $(this).parent().parent().find('.views_slideshow_main');
+
+      // Remove the views slideshow xtra html from the dom
+      var xtraHTML = $(this).detach();
+
+      // Attach the views slideshow xtra html to below the main slide.
+      $(xtraHTML).appendTo(slideArea);
+
+      // Get the offsets of the slide and the views slideshow xtra elements
+      var slideAreaOffset = slideArea.offset();
+      var xtraOffset = $(this).offset();
+
+      // Move the views slideshow xtra element over the top of the slide.
+      var marginMove = slideAreaOffset.top - xtraOffset.top;
+      $(this).css({'margin-top': marginMove + 'px'});
+
+      // Move type=text slide elements into place.
+      var slideData = Drupal.settings.viewsSlideshowXtra[options.slideshowID].slideInfo.text;
+      var slideNum = 0;
+      for (slide in slideData) {
+        var items = slideData[slide];
+        var itemNum = 0;
+        for (item in items) {
+          //alert('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-text-' + slideNum + '-' + itemNum);
+          $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-text-' + slideNum + '-' + itemNum).css({
+            'width': slideArea.width()
+          });
+          itemNum++;
+        }
+        slideNum++;
+      }
+
+      // Move type=link slide elements into place.
+      var slideData = Drupal.settings.viewsSlideshowXtra[options.slideshowID].slideInfo.link;
+      var slideNum = 0;
+      for (slide in slideData) {
+        var items = slideData[slide];
+        var itemNum = 0;
+        for (item in items) {
+          //alert('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-link-' + slideNum + '-' + itemNum);
+        	$('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-link-' + slideNum + '-' + itemNum).css({
+            'width': slideArea.width()
+          });
+          itemNum++;
+        }
+        slideNum++;
+      }
+
+      // Move type=image slide elements into place.
+      var slideData = Drupal.settings.viewsSlideshowXtra[options.slideshowID].slideInfo.image;
+      var slideNum = 0;
+      for (slide in slideData) {
+        var items = slideData[slide];
+        var itemNum = 0;
+        for (item in items) {
+          //alert('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-image-' + slideNum + '-' + itemNum);
+        	$('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-image-' + slideNum + '-' + itemNum).css({
+            'width': slideArea.width()
+          });
+          itemNum++;
+        }
+        slideNum++;
+      }
+
+      var settings = Drupal.settings.viewsSlideshowXtra[options.slideshowID];
+	    if (settings.pauseAfterMouseMove) {
+	    //if(true) {
+	    	slideArea.mousemove(function(e) {
+      	  if (pageX - e.pageX > 5 || pageY - e.pageY > 5) {
+      	  	Drupal.viewsSlideshow.action({ "action": 'pause', "slideshowID": options.slideshowID });
+      	    clearTimeout(timeout);
+      	    timeout = setTimeout(function() {
+      	    		Drupal.viewsSlideshow.action({ "action": 'play', "slideshowID": options.slideshowID });
+      	    		}, 2000);
+      	  }
+      	  pageX = e.pageX;
+      	  pageY = e.pageY;
+	    	});
+	    }
+
+    });
+
+    // TODO Find a better way to detect if xtra module is enabled but this is not
+    // an xtra slideshow.  This seems to work but its probably not the right way.
+    //if (Drupal.settings.viewsSlideshowXtra[options.slideshowID]) { THIS DOES NOT WORK!
+    if ('viewsSlideshowXtra' in Drupal.settings) {
+	    var settings = Drupal.settings.viewsSlideshowXtra[options.slideshowID];
+
+	    // Hide elements either by fading or not
+	    if (settings.displayDelayFade) {
+	      $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row').fadeOut();
+	    }
+	    else {
+	      $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row').hide();
+	    }
+
+      settings.currentTransitioningSlide = options.slideNum;
+
+	    // Pause from showing the text and links however long the user decides.
+	    setTimeout(function() {
+        if (options.slideNum == settings.currentTransitioningSlide) {
+          if (settings.displayDelayFade) {
+            $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row-' + options.slideNum).fadeIn();
+          }
+          else {
+            $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row-' + options.slideNum).show();
+          }
+        }
+	    },
+	    settings.displayDelay
+	    );
+
+    }
+  };
+})(jQuery);
+
+/*
+*/
+;
+(function($) {
+
+Drupal.admin = Drupal.admin || {};
+Drupal.admin.behaviors = Drupal.admin.behaviors || {};
+
+/**
+ * @ingroup admin_behaviors
+ * @{
+ */
+
+/**
+ * Apply active trail highlighting based on current path.
+ *
+ * @todo Not limited to toolbar; move into core?
+ */
+Drupal.admin.behaviors.toolbarActiveTrail = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.toolbar && settings.admin_menu.toolbar.activeTrail) {
+    $adminMenu.find('> div > ul > li > a[href="' + settings.admin_menu.toolbar.activeTrail + '"]').addClass('active-trail');
+  }
+};
+
+/**
+ * @} End of "ingroup admin_behaviors".
+ */
+
+Drupal.admin.behaviors.shorcutcollapsed = function (context, settings, $adminMenu) {
+
+  // Create the dropdown base 
+  $("<li class=\"label\"><a>"+Drupal.t('Shortcuts')+"</a></li>").prependTo("body.menu-render-collapsed div.toolbar-shortcuts ul"); 
+
+}
+
+Drupal.admin.behaviors.shorcutselect = function (context, settings, $adminMenu) {
+
+  // Create the dropdown base
+  $("<select id='shortcut-menu'/>").appendTo("body.menu-render-dropdown div.toolbar-shortcuts");
+    
+  // Create default option "Select"
+  $("<option />", {
+    "selected"  :  "selected",
+    "value"     :  "",
+    "text"      :  Drupal.t('Shortcuts')
+  }).appendTo("body.menu-render-dropdown div.toolbar-shortcuts select");
+    
+  // Populate dropdown with menu items
+  $("body.menu-render-dropdown div.toolbar-shortcuts a").each(function() {
+    var el = $(this);
+    $("<option />", {
+      "value"   :  el.attr("href"),
+      "text"    :  el.text()
+    }).appendTo("body.menu-render-dropdown div.toolbar-shortcuts select");
+    });
+    
+  $("body.menu-render-dropdown div.toolbar-shortcuts select").change(function() {
+    window.location = $(this).find("option:selected").val();
+  });
+  
+  $('body.menu-render-dropdown div.toolbar-shortcuts ul').remove();
+
+};
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.dateAdmin = {};
+
+Drupal.behaviors.dateAdmin.attach = function (context, settings) {
+  // Remove timezone handling options for fields without hours granularity.
+  var $hour = $('#edit-field-settings-granularity-hour').once('date-admin');
+  if ($hour.length) {
+    new Drupal.dateAdmin.TimezoneHandler($hour);
+  }
+};
+
+
+Drupal.dateAdmin = {};
+
+/**
+ * Constructor for the TimezoneHandler object.
+ *
+ * This object is responsible for showing the timezone handling options dropdown
+ * when the user has chosen to collect hours as part of the date field, and
+ * hiding it otherwise.
+ */
+Drupal.dateAdmin.TimezoneHandler = function ($checkbox) {
+  this.$checkbox = $checkbox;
+  this.$dropdown = $('#edit-field-settings-tz-handling');
+  this.$timezoneDiv = $('.form-item-field-settings-tz-handling');
+  // Store the initial value of the timezone handling dropdown.
+  this.storeTimezoneHandling();
+  // Toggle the timezone handling section when the user clicks the "Hour"
+  // checkbox.
+  this.$checkbox.bind('click', $.proxy(this.clickHandler, this));
+  // Trigger the click handler so that if the checkbox is unchecked on initial
+  // page load, the timezone handling section will be hidden.
+  this.clickHandler();
+};
+
+/**
+ * Event handler triggered when the user clicks the "Hour" checkbox.
+ */
+Drupal.dateAdmin.TimezoneHandler.prototype.clickHandler = function () {
+  if (this.$checkbox.is(':checked')) {
+    this.restoreTimezoneHandlingOptions();
+  }
+  else {
+    this.hideTimezoneHandlingOptions();
+  }
+};
+
+/**
+ * Hide the timezone handling options section of the form.
+ */
+Drupal.dateAdmin.TimezoneHandler.prototype.hideTimezoneHandlingOptions = function () {
+  this.storeTimezoneHandling();
+  this.$dropdown.val('none');
+  this.$timezoneDiv.hide();
+};
+
+/**
+ * Show the timezone handling options section of the form.
+ */
+Drupal.dateAdmin.TimezoneHandler.prototype.restoreTimezoneHandlingOptions = function () {
+  var val = this.getTimezoneHandling();
+  this.$dropdown.val(val);
+  this.$timezoneDiv.show();
+};
+
+/**
+ * Store the current value of the timezone handling dropdown.
+ */
+Drupal.dateAdmin.TimezoneHandler.prototype.storeTimezoneHandling = function () {
+  this._timezoneHandling = this.$dropdown.val();
+};
+
+/**
+ * Return the stored value of the timezone handling dropdown.
+ */
+Drupal.dateAdmin.TimezoneHandler.prototype.getTimezoneHandling = function () {
+  return this._timezoneHandling;
+};
+
+})(jQuery);
+;
---
+++ sites/default/files/js/js_P4lysFO7sZRd1H6XnVbeT5QgkRjzTyP31ztdKZukfxw.js
@@ -0,0 +1,323 @@
+/**
+ * @file
+ * Views Slideshow Xtra Javascript.
+ */
+(function ($) {
+  Drupal.viewsSlideshowXtra = Drupal.viewsSlideshowXtra || {};
+  var pageX = 0, pageY = 0, timeout;
+  Drupal.viewsSlideshowXtra.transitionBegin = function (options) {
+
+    // Find our views slideshow xtra elements
+    $('[id^="views-slideshow-xtra-"]:not(.views-slideshow-xtra-processed)').addClass('views-slideshow-xtra-processed').each(function() {
+
+      // Get the slide box.
+      var slideArea = $(this).parent().parent().find('.views_slideshow_main');
+
+      // Remove the views slideshow xtra html from the dom
+      var xtraHTML = $(this).detach();
+
+      // Attach the views slideshow xtra html to below the main slide.
+      $(xtraHTML).appendTo(slideArea);
+
+      // Get the offsets of the slide and the views slideshow xtra elements
+      var slideAreaOffset = slideArea.offset();
+      var xtraOffset = $(this).offset();
+
+      // Move the views slideshow xtra element over the top of the slide.
+      var marginMove = slideAreaOffset.top - xtraOffset.top;
+      $(this).css({'margin-top': marginMove + 'px'});
+
+      // Move type=text slide elements into place.
+      var slideData = Drupal.settings.viewsSlideshowXtra[options.slideshowID].slideInfo.text;
+      var slideNum = 0;
+      for (slide in slideData) {
+        var items = slideData[slide];
+        var itemNum = 0;
+        for (item in items) {
+          //alert('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-text-' + slideNum + '-' + itemNum);
+          $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-text-' + slideNum + '-' + itemNum).css({
+            'width': slideArea.width()
+          });
+          itemNum++;
+        }
+        slideNum++;
+      }
+
+      // Move type=link slide elements into place.
+      var slideData = Drupal.settings.viewsSlideshowXtra[options.slideshowID].slideInfo.link;
+      var slideNum = 0;
+      for (slide in slideData) {
+        var items = slideData[slide];
+        var itemNum = 0;
+        for (item in items) {
+          //alert('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-link-' + slideNum + '-' + itemNum);
+        	$('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-link-' + slideNum + '-' + itemNum).css({
+            'width': slideArea.width()
+          });
+          itemNum++;
+        }
+        slideNum++;
+      }
+
+      // Move type=image slide elements into place.
+      var slideData = Drupal.settings.viewsSlideshowXtra[options.slideshowID].slideInfo.image;
+      var slideNum = 0;
+      for (slide in slideData) {
+        var items = slideData[slide];
+        var itemNum = 0;
+        for (item in items) {
+          //alert('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-image-' + slideNum + '-' + itemNum);
+        	$('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-image-' + slideNum + '-' + itemNum).css({
+            'width': slideArea.width()
+          });
+          itemNum++;
+        }
+        slideNum++;
+      }
+
+      var settings = Drupal.settings.viewsSlideshowXtra[options.slideshowID];
+	    if (settings.pauseAfterMouseMove) {
+	    //if(true) {
+	    	slideArea.mousemove(function(e) {
+      	  if (pageX - e.pageX > 5 || pageY - e.pageY > 5) {
+      	  	Drupal.viewsSlideshow.action({ "action": 'pause', "slideshowID": options.slideshowID });
+      	    clearTimeout(timeout);
+      	    timeout = setTimeout(function() {
+      	    		Drupal.viewsSlideshow.action({ "action": 'play', "slideshowID": options.slideshowID });
+      	    		}, 2000);
+      	  }
+      	  pageX = e.pageX;
+      	  pageY = e.pageY;
+	    	});
+	    }
+
+    });
+
+    // TODO Find a better way to detect if xtra module is enabled but this is not
+    // an xtra slideshow.  This seems to work but its probably not the right way.
+    //if (Drupal.settings.viewsSlideshowXtra[options.slideshowID]) { THIS DOES NOT WORK!
+    if ('viewsSlideshowXtra' in Drupal.settings) {
+	    var settings = Drupal.settings.viewsSlideshowXtra[options.slideshowID];
+
+	    // Hide elements either by fading or not
+	    if (settings.displayDelayFade) {
+	      $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row').fadeOut();
+	    }
+	    else {
+	      $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row').hide();
+	    }
+
+      settings.currentTransitioningSlide = options.slideNum;
+
+	    // Pause from showing the text and links however long the user decides.
+	    setTimeout(function() {
+        if (options.slideNum == settings.currentTransitioningSlide) {
+          if (settings.displayDelayFade) {
+            $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row-' + options.slideNum).fadeIn();
+          }
+          else {
+            $('#views-slideshow-xtra-' + options.slideshowID + ' .views-slideshow-xtra-row-' + options.slideNum).show();
+          }
+        }
+	    },
+	    settings.displayDelay
+	    );
+
+    }
+  };
+})(jQuery);
+
+/*
+*/
+;
+(function($) {
+
+Drupal.admin = Drupal.admin || {};
+Drupal.admin.behaviors = Drupal.admin.behaviors || {};
+
+/**
+ * @ingroup admin_behaviors
+ * @{
+ */
+
+/**
+ * Apply active trail highlighting based on current path.
+ *
+ * @todo Not limited to toolbar; move into core?
+ */
+Drupal.admin.behaviors.toolbarActiveTrail = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.toolbar && settings.admin_menu.toolbar.activeTrail) {
+    $adminMenu.find('> div > ul > li > a[href="' + settings.admin_menu.toolbar.activeTrail + '"]').addClass('active-trail');
+  }
+};
+
+/**
+ * @} End of "ingroup admin_behaviors".
+ */
+
+Drupal.admin.behaviors.shorcutcollapsed = function (context, settings, $adminMenu) {
+
+  // Create the dropdown base 
+  $("<li class=\"label\"><a>"+Drupal.t('Shortcuts')+"</a></li>").prependTo("body.menu-render-collapsed div.toolbar-shortcuts ul"); 
+
+}
+
+Drupal.admin.behaviors.shorcutselect = function (context, settings, $adminMenu) {
+
+  // Create the dropdown base
+  $("<select id='shortcut-menu'/>").appendTo("body.menu-render-dropdown div.toolbar-shortcuts");
+    
+  // Create default option "Select"
+  $("<option />", {
+    "selected"  :  "selected",
+    "value"     :  "",
+    "text"      :  Drupal.t('Shortcuts')
+  }).appendTo("body.menu-render-dropdown div.toolbar-shortcuts select");
+    
+  // Populate dropdown with menu items
+  $("body.menu-render-dropdown div.toolbar-shortcuts a").each(function() {
+    var el = $(this);
+    $("<option />", {
+      "value"   :  el.attr("href"),
+      "text"    :  el.text()
+    }).appendTo("body.menu-render-dropdown div.toolbar-shortcuts select");
+    });
+    
+  $("body.menu-render-dropdown div.toolbar-shortcuts select").change(function() {
+    window.location = $(this).find("option:selected").val();
+  });
+  
+  $('body.menu-render-dropdown div.toolbar-shortcuts ul').remove();
+
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Attach the machine-readable name form element behavior.
+ */
+Drupal.behaviors.machineName = {
+  /**
+   * Attaches the behavior.
+   *
+   * @param settings.machineName
+   *   A list of elements to process, keyed by the HTML ID of the form element
+   *   containing the human-readable value. Each element is an object defining
+   *   the following properties:
+   *   - target: The HTML ID of the machine name form element.
+   *   - suffix: The HTML ID of a container to show the machine name preview in
+   *     (usually a field suffix after the human-readable name form element).
+   *   - label: The label to show for the machine name preview.
+   *   - replace_pattern: A regular expression (without modifiers) matching
+   *     disallowed characters in the machine name; e.g., '[^a-z0-9]+'.
+   *   - replace: A character to replace disallowed characters with; e.g., '_'
+   *     or '-'.
+   *   - standalone: Whether the preview should stay in its own element rather
+   *     than the suffix of the source element.
+   *   - field_prefix: The #field_prefix of the form element.
+   *   - field_suffix: The #field_suffix of the form element.
+   */
+  attach: function (context, settings) {
+    var self = this;
+    $.each(settings.machineName, function (source_id, options) {
+      var $source = $(source_id, context).addClass('machine-name-source');
+      var $target = $(options.target, context).addClass('machine-name-target');
+      var $suffix = $(options.suffix, context);
+      var $wrapper = $target.closest('.form-item');
+      // All elements have to exist.
+      if (!$source.length || !$target.length || !$suffix.length || !$wrapper.length) {
+        return;
+      }
+      // Skip processing upon a form validation error on the machine name.
+      if ($target.hasClass('error')) {
+        return;
+      }
+      // Figure out the maximum length for the machine name.
+      options.maxlength = $target.attr('maxlength');
+      // Hide the form item container of the machine name form element.
+      $wrapper.hide();
+      // Determine the initial machine name value. Unless the machine name form
+      // element is disabled or not empty, the initial default value is based on
+      // the human-readable form element value.
+      if ($target.is(':disabled') || $target.val() != '') {
+        var machine = $target.val();
+      }
+      else {
+        var machine = self.transliterate($source.val(), options);
+      }
+      // Append the machine name preview to the source field.
+      var $preview = $('<span class="machine-name-value">' + options.field_prefix + Drupal.checkPlain(machine) + options.field_suffix + '</span>');
+      $suffix.empty();
+      if (options.label) {
+        $suffix.append(' ').append('<span class="machine-name-label">' + options.label + ':</span>');
+      }
+      $suffix.append(' ').append($preview);
+
+      // If the machine name cannot be edited, stop further processing.
+      if ($target.is(':disabled')) {
+        return;
+      }
+
+      // If it is editable, append an edit link.
+      var $link = $('<span class="admin-link"><a href="#">' + Drupal.t('Edit') + '</a></span>')
+        .click(function () {
+          $wrapper.show();
+          $target.focus();
+          $suffix.hide();
+          $source.unbind('.machineName');
+          return false;
+        });
+      $suffix.append(' ').append($link);
+
+      // Preview the machine name in realtime when the human-readable name
+      // changes, but only if there is no machine name yet; i.e., only upon
+      // initial creation, not when editing.
+      if ($target.val() == '') {
+        $source.bind('keyup.machineName change.machineName input.machineName', function () {
+          machine = self.transliterate($(this).val(), options);
+          // Set the machine name to the transliterated value.
+          if (machine != '') {
+            if (machine != options.replace) {
+              $target.val(machine);
+              $preview.html(options.field_prefix + Drupal.checkPlain(machine) + options.field_suffix);
+            }
+            $suffix.show();
+          }
+          else {
+            $suffix.hide();
+            $target.val(machine);
+            $preview.empty();
+          }
+        });
+        // Initialize machine name preview.
+        $source.keyup();
+      }
+    });
+  },
+
+  /**
+   * Transliterate a human-readable name to a machine name.
+   *
+   * @param source
+   *   A string to transliterate.
+   * @param settings
+   *   The machine name settings for the corresponding field, containing:
+   *   - replace_pattern: A regular expression (without modifiers) matching
+   *     disallowed characters in the machine name; e.g., '[^a-z0-9]+'.
+   *   - replace: A character to replace disallowed characters with; e.g., '_'
+   *     or '-'.
+   *   - maxlength: The maximum length of the machine name.
+   *
+   * @return
+   *   The transliterated source string.
+   */
+  transliterate: function (source, settings) {
+    var rx = new RegExp(settings.replace_pattern, 'g');
+    return source.toLowerCase().replace(rx, settings.replace).substr(0, settings.maxlength);
+  }
+};
+
+})(jQuery);
+;
